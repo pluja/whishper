6f686085f2aca70f37c1c4f25c105a06ae9c9314
pluja <github_pluja@r3d.red>
2024-03-08 09:03:37 +0100

    add subtitles config

---
+++ backend/utils/subtitles/subtitles.go
@@ -0,0 +1,7 @@
+package subtitles
+
+type SubtitleConfig struct {
+	ColorizeSpeakers bool              `json:"colorize_speakers"`
+	MaxLengthChars   int               `json:"max_length_chars"`
+	SpeakerColors    map[string]string `json:"speaker_colors"`
+}
