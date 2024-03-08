1859baff167018bd653426a5cdf0d110d778d84c
pluja <github_pluja@r3d.red>
2024-03-08 09:03:51 +0100

    remove old module subtitles stuff

--- backend/utils/subtitles.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-type SubtitleConfig struct {
-	ColorizeSpeakers bool              `json:"colorize_speakers"`
-	MaxLengthChars   int               `json:"max_length_chars"`
-	SpeakerColors    map[string]string `json:"speaker_colors"`
-}
