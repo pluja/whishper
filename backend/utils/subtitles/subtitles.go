8784d659a6bd6d61b521142c57bcf8152202a7f5
pluja <github_pluja@r3d.red>
2024-03-08 09:26:11 +0100

    add more config to subs

--- backend/utils/subtitles/subtitles.go
+++ backend/utils/subtitles/subtitles.go
@@ -4,4 +4,6 @@ type SubtitleConfig struct {
 	ColorizeSpeakers bool              `json:"colorize_speakers"`
 	MaxLengthChars   int               `json:"max_length_chars"`
 	SpeakerColors    map[string]string `json:"speaker_colors"`
+	MsDelay          int64             `json:"delay_ms"`
+	MaxTimeGap       int64             `json:"max_gap_ms"`
 }
